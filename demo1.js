c51be7e1b1bf4a4945a94418092d2e927e2584a1
Divyanshu <140911251+learneroflife30@users.noreply.github.com>
2024-06-09 08:57:58 +0530

    Add files via upload

---
+++ demo1.js
@@ -0,0 +1,20 @@
+var age1 = 30; // number data type
+var name1 = "Divyanshu"; // name data type
+var team = ["david", "john"];
+var scores = [23, 89, 31];
+var ispresent = false; // boolean data type
+var city = "Norway, oslo"; // any data type
+console.log(age1);
+console.log(name1);
+console.log(team);
+console.log(scores);
+console.log(ispresent);
+console.log(city);
+// functions
+function test(city, states, UT) {
+    return (city + states + UT);
+}
+(400 + 28 + 8);
+// objects
+var teamdata = { experience: 6, members: 5, nameing: 'all' };
+console.log(teamdata);
---
+++ demo1.ts
@@ -0,0 +1,28 @@
+let age1: number = 30;    // number data type
+let name1: string = "Divyanshu";  // name data type
+let team: string[]=["david", "john"];
+let scores: number[]= [23,89,31];
+let ispresent:boolean = false; // boolean data type
+let city:any="Norway, oslo" // any data type
+
+console.log(age1)
+console.log(name1)
+console.log(team)
+console.log(scores)
+console.log(ispresent)
+console.log(city)
+
+
+// functions
+function test(city:number, states:number, UT:number):any
+{
+
+    return (city + states + UT);
+
+}
+        (400 + 28 + 8);
+
+
+// objects
+let teamdata: {experience: number, members:number, nameing:string} ={experience:6, members:5, nameing:'all'}
+console.log(teamdata);
